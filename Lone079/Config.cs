751d29c9c22686c4b70563f3688fd88254c6673b
Zenlet <62112167+Zenlet@users.noreply.github.com>
2022-04-15 18:51:58 +0100

    config descriptions

--- Lone079/Config.cs
+++ Lone079/Config.cs
@@ -1,15 +1,21 @@
 ﻿namespace Lone079
 {
     using System.Collections.Generic;
-    using Exiled.API.Features;
+	using System.ComponentModel;
+	using Exiled.API.Features;
     using Exiled.API.Interfaces;
 
     public class Config : IConfig
     {
+        [Description("Enables or disables the plugin")]
         public bool IsEnabled { get; set; } = true;
+        [Description("Determines if zombies should be counted when determining if SCP-079 is the last SCP alive.")]
         public bool CountZombies { get; set; } = false;
+        [Description("The percentage of normal health SCP-079 should have when he respawns as a random SCP.")]
         public int HealthPercent { get; set; } = 50;
+        [Description("The message shown to the player when they have spawned in as a different scp.")]
         public Broadcast Broadcast { get; set; } = new Broadcast("<i>You have been respawned as a random SCP with half health because all other SCPs have died.</i>");
+        [Description("Determines what SCP079 will be respawned as after the last scp dies.")]
         public List<RoleType> Scp079Respawns { get; set; } = new List<RoleType>
         {
             RoleType.Scp173,
@@ -19,6 +25,7 @@
             RoleType.Scp93953,
             RoleType.Scp93989,
         };
+        [Description("Determines where scp079 will be respawned. Only accepts scp names.")]
         public List<RoleType> Scp079RespawnLocations { get; set; } = new List<RoleType>
         {
             RoleType.Scp049,
--- README.md
+++ README.md
@@ -15,6 +15,5 @@ Place the "Lone079.dll" file in your EXILED/Plugins folder.
 | 079_count_zombies | Boolean | False | Determines if zombies should be counted when determining if SCP-079 is the last SCP alive. |
 | 079_health_percentage | Integer | 50 | The percentage of normal health SCP-079 should have when he respawns as a random SCP. |
 | Broadcast_Message | String | <i>You have been respawned as a random SCP with half health because all other SCPs have died.</i> | The message shown to the player when they have spawned in as a different scp. |
-| BroadcastTime | Integer | 10 | Broadcast_Message time |
 | scp079_respawns | List |  | Determines what SCP079 will be respawned as after the last scp dies. |
 | scp079_respawn_locations | List |  | Determines where scp079 will be respawned. Only accepts scp names. |
